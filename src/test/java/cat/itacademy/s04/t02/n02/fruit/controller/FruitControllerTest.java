2318ec926d620f626528d9fb270231faa7a4fcaf
erictc95 <maxerictc95@hotmail.com>
2026-06-09 14:59:12 +0200

    feat: Fruit Tests.

---
+++ src/test/java/cat/itacademy/s04/t02/n02/fruit/controller/FruitControllerTest.java
@@ -0,0 +1,73 @@
+package cat.itacademy.s04.t02.n02.fruit.controller;
+
+import cat.itacademy.s04.t02.n02.fruit.dto.CreateFruitRequest;
+import cat.itacademy.s04.t02.n02.fruit.dto.FruitResponse;
+import cat.itacademy.s04.t02.n02.fruit.service.FruitService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(FruitController.class)
+public class FruitControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private FruitService fruitService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void shouldReturnFruitById() throws Exception {
+        FruitResponse response = new FruitResponse(1L, "Apple", 10, 1L, "Fresh Fruits");
+
+        when(fruitService.getFruitById(1L)).thenReturn(response);
+
+        mockMvc.perform(get("/fruits/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Apple"))
+                .andExpect(jsonPath("$.weightInKilos").value(10));
+    }
+
+    @Test
+    void shouldReturnAllFruits() throws Exception {
+
+        List<FruitResponse> fruits = List.of(
+                new FruitResponse(1L, "Apple", 10, 1L, "Fresh Fruits"),
+                new FruitResponse(2L, "Banana", 8, 1L, "Fresh Fruits"));
+
+        when(fruitService.getAllFruits()).thenReturn(fruits);
+
+        mockMvc.perform(get("/fruits"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2));
+    }
+
+    @Test
+    void shouldCreateFruit() throws Exception {
+        CreateFruitRequest request = new CreateFruitRequest("Banana", 8, 1L);
+
+        FruitResponse response = new FruitResponse(1L, "Banana", 8, 1L, "Fresh Fruits");
+
+        when(fruitService.createFruit(request)).thenReturn(response);
+
+        mockMvc.perform(post("/fruits")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value("Banana"));
+    }
+}
---
+++ src/test/java/cat/itacademy/s04/t02/n02/fruit/service/FruitServiceImplTest.java
@@ -0,0 +1,120 @@
+package cat.itacademy.s04.t02.n02.fruit.service;
+
+import cat.itacademy.s04.t02.n02.fruit.dto.CreateFruitRequest;
+import cat.itacademy.s04.t02.n02.fruit.dto.UpdateFruitRequest;
+import cat.itacademy.s04.t02.n02.fruit.exception.FruitNotFoundException;
+import cat.itacademy.s04.t02.n02.fruit.model.Fruit;
+import cat.itacademy.s04.t02.n02.fruit.model.Provider;
+import cat.itacademy.s04.t02.n02.fruit.repository.FruitRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class FruitServiceImplTest {
+
+    @Mock
+    private FruitRepository fruitRepository;
+
+    @InjectMocks
+    private FruitServiceImpl fruitService;
+
+    @Test
+    void shouldCreateFruitSuccessfully() {
+        CreateFruitRequest request = new CreateFruitRequest("Apple", 10, 1L);
+
+        Fruit savedFruit = new Fruit("Apple", 10, new Provider("Fresh Fruits", "Spain"));
+
+        when(fruitRepository.save(any(Fruit.class))).thenReturn(savedFruit);
+
+        var response = fruitService.createFruit(request);
+
+        assertEquals("Apple", response.name());
+        assertEquals(10, response.weightInKilos());
+
+        verify(fruitRepository).save(any(Fruit.class));
+    }
+
+    @Test
+    void shouldReturnFruitById() {
+        Fruit fruit = new Fruit("Banana", 15, new Provider("Fresh Fruits", "Spain"));
+
+        when(fruitRepository.findById(1L)).thenReturn(Optional.of(fruit));
+
+        var response = fruitService.getFruitById(1L);
+
+        assertEquals("Banana", response.name());
+        assertEquals(15, response.weightInKilos());
+
+        verify(fruitRepository).findById(1L);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenFruitNotFound() {
+        when(fruitRepository.findById(1L)).thenReturn(Optional.empty());
+
+        assertThrows(FruitNotFoundException.class, () -> fruitService.getFruitById(1L));
+    }
+
+    @Test
+    void shouldReturnAllFrits() {
+        List<Fruit> fruits = List.of(
+                new Fruit("Apple", 10, new Provider("Fresh Fruits", "Spain")),
+                new Fruit("Banana", 15, new Provider("Fresh Fruits", "Spain"))
+        );
+
+        when(fruitRepository.findAll()).thenReturn(fruits);
+
+        var response = fruitService.getAllFruits();
+
+        assertEquals(2, response.size());
+
+        assertEquals("Apple", response.get(0).name());
+        assertEquals("Banana", response.get(1).name());
+
+        verify(fruitRepository).findAll();
+    }
+
+    @Test
+    void shouldUpdateFruit() {
+        Fruit fruit = new Fruit("Apple", 10, new Provider("Fresh Fruits", "Spain"));
+
+        UpdateFruitRequest request = new UpdateFruitRequest("Green Apple", 15,1L);
+
+        when(fruitRepository.findById(1L)).thenReturn(Optional.of(fruit));
+
+        when(fruitRepository.save(any(Fruit.class))).thenReturn(fruit);
+
+        var response = fruitService.updateFruit(1L, request);
+
+        assertEquals("Green Apple", response.name());
+        assertEquals(15, response.weightInKilos());
+
+        verify(fruitRepository).findById(1L);
+        verify(fruitRepository).save(any(Fruit.class));
+    }
+
+    @Test
+    void shouldDeleteFruit() {
+        Fruit fruit = new Fruit("Orange", 5, new Provider("Fresh Fruits", "Spain"));
+
+        when(fruitRepository.findById(1L)).thenReturn(Optional.of(fruit));
+
+        fruitService.deleteFruit(1L);
+
+        verify(fruitRepository).findById(1L);
+        verify(fruitRepository).delete(fruit);
+    }
+
+}
